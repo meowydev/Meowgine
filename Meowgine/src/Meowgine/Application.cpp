a303056d977caa16a9b71c991750bf0bfa75c0e8
MeowyDev <meowy@meowerity.com>
2026-03-30 00:03:38 +0900

    Vertex Buffer Layout implemented
    Finally after 2 weeks it was implemented.

--- Meowgine/src/Meowgine/Application.cpp
+++ Meowgine/src/Meowgine/Application.cpp
@@ -7,6 +7,26 @@
 namespace Meowgine {
 #define BIND_EVENT_FUNC(x) std::bind(&Application::x, this, std::placeholders::_1)
 	Application* Application::s_Instance = nullptr;
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type) {
+		switch (type)
+		{
+			case Meowgine::ShaderDataType::Float:	return GL_FLOAT;
+			case Meowgine::ShaderDataType::Float2:	return GL_FLOAT;
+			case Meowgine::ShaderDataType::Float3:	return GL_FLOAT;
+			case Meowgine::ShaderDataType::Float4:	return GL_FLOAT;
+			case Meowgine::ShaderDataType::Mat3:	return GL_FLOAT;
+			case Meowgine::ShaderDataType::Mat4:	return GL_FLOAT;
+			case Meowgine::ShaderDataType::Int:		return GL_INT;
+			case Meowgine::ShaderDataType::Int2:	return GL_INT;
+			case Meowgine::ShaderDataType::Int3:	return GL_INT;
+			case Meowgine::ShaderDataType::Int4:	return GL_INT;
+			case Meowgine::ShaderDataType::Bool:	return GL_BOOL;
+		}
+		// crash the shit out of it if it was used wrong
+		MG_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+
+	}
 	Application::Application() {
 		MG_CORE_ASSERT(!s_Instance, "Application already exists");
 		s_Instance = this;
@@ -19,26 +39,35 @@ namespace Meowgine {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float verticies[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
+		float verticies[3 * 7] = {
+			//X     Y     Z     R     G     B     A
+			-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Bottom left
+			 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, // Bottom right
+			 0.0f,	0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f  // Top
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(verticies, sizeof(verticies)));
 		
-		BufferLayout layout = {
-			{ ShaderDataType::Float3, "a_Position" }
-		};
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position"},
+				{ ShaderDataType::Float4, "a_Color"}
+			};
 
-		uint32_t index = 0;
-
-		for (const auto& element : layout) {
-			glEnableVertexAttribArray(index);
-			glVertexAttribPointer(index, element.GetComponentCount(), GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
-			index++;
+			m_VertexBuffer->SetLayout(layout);
 		}
 
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout) {
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 		
 
 		unsigned int indices[3] = {
@@ -50,12 +79,16 @@ namespace Meowgine {
 		#version 330 core
 		
 		layout(location = 0) in vec3 a_Position;
+		layout(location = 1) in vec4 a_Color;
+		
 		
 		out vec3 v_Position;
+		out vec4 v_Color;
 		
 		void main()
 		{
 			v_Position = a_Position;
+			v_Color = a_Color;
 			gl_Position = vec4(a_Position,1.0);
 		}
 		
@@ -67,10 +100,12 @@ namespace Meowgine {
 		layout(location = 0) out vec4 color;
 		
 		in vec3 v_Position;
+		in vec4 v_Color;
 		
 		void main()
 		{
 			color = vec4(v_Position * 0.5 + 0.5,1.0);
+			color = v_Color;
 		}
 		
 		)";
--- Meowgine/src/Meowgine/Renderer/Buffer.h
+++ Meowgine/src/Meowgine/Renderer/Buffer.h
@@ -37,7 +37,11 @@ namespace Meowgine
 		uint32_t Size;
 		uint32_t Offset;
 		
-		BufferElement( ShaderDataType type, const std::string& name ) : Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0)
+		bool Normalized;
+		
+		BufferElement() {}
+
+		BufferElement( ShaderDataType type, const std::string& name, bool normalized = false) : Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0) , Normalized(normalized)
 		{
 
 		}
@@ -66,13 +70,18 @@ namespace Meowgine
 	class BufferLayout
 	{
 	public:
+		BufferLayout() {}
+
 		BufferLayout(const std::initializer_list<BufferElement>& elements) : m_Elements(elements) {
 			CalculateOffsetsAndStride();
 		}
+		inline uint32_t GetStride() const { return m_Stride; }
 		inline const std::vector<BufferElement>& GetElements() const { return m_Elements; }
 
 		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
 		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
+		std::vector<BufferElement>::const_iterator begin() const { return m_Elements.begin(); }
+		std::vector<BufferElement>::const_iterator end() const { return m_Elements.end(); }
 	private:
 		void CalculateOffsetsAndStride()
 		{
